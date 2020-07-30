6091d7e9eb4d4ad7a00926c164edb384c4ddbbbc
nobucastle <30787155+nobucastle@users.noreply.github.com>
2020-07-30 15:56:19 +0900

    Update index.php

--- index.php
+++ index.php
@@ -10,5 +10,9 @@ $dsn = 'pgsql:host=' . $dbinfo['host'] . ';dbname=' . substr($dbinfo['path'], 1)
 $pdo = new PDO($dsn, $dbinfo['user'], $dbinfo['pass']);
 var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 
+$stmt = $pdo->query('SELECT user1, user2 FROM user_tb');
+$result = $stmt->fetchAll();
+return $result;
+
 phpinfo();
 ?>
