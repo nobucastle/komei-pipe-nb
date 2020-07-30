bb7ef073a30a4eda3e3b3544946c02d798a47857
nobucastle <30787155+nobucastle@users.noreply.github.com>
2020-07-30 15:36:44 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@ First Release
 
 <?
 $url = parse_url(getenv('DATABASE_URL'));
-
 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
-
 $pdo = new PDO($dsn, $url['user'], $url['pass']);
-var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 
-phpinfo();
+
+$stmt = $pdo->prepare("SELECT * FROM user_tb");
+$stmt->execute([$user1]);
+$result = $stmt->fetch();
 ?>
